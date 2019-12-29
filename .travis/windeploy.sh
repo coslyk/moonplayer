fb410df8845749eeb0610765a1c3a9d73611ac38
coslyk <cos.lyk@gmail.com>
2019-12-29 14:45:16 +0100

    travis: fix path of iscc

--- .travis/windeploy.sh
+++ .travis/windeploy.sh
@@ -7,5 +7,5 @@ cp src/Release/moonplayer.exe src/scripts/update-parsers.ps1 libmpv/mpv-1.dll mo
 windeployqt moonplayer/moonplayer.exe --qmldir src/qml
 pyinstaller -F src/scripts/hls_downloader.py --distpath moonplayer
 pyinstaller -F src/scripts/danmaku2ass.py --distpath moonplayer
-issc .travis/win_installer.iss
+iscc .travis/win_installer.iss
 mv Output/mysetup.exe ./MoonPlayer_${TRAVIS_TAG}_win_x64.exe
