3c8642977bf977e4ae59821ab7b030f1f2168407
YK Liu <cos.lyk@gmail.com>
2019-12-29 13:27:48 +0100

    travis: add windows deploy

---
+++ .travis/windeploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export PATH=/c/Qt/5.12.3/msvc2017_64/bin:$PATH
+
+mkdir moonplayer
+cp src/moonplayer.exe src/scripts/update-parsers.ps1 libmpv/mpv-1.dll moonplayer/
+windeployqt moonplayer/moonplayer.exe --qmldir src/qml
+zip -9 -r MoonPlayer_${TRAVIS_TAG}_windows_x64.zip moonplayer
