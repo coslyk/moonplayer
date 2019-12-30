7362f09f53b774804ac4ec42f14c107201997e7d
YK Liu <cos.lyk@gmail.com>
2019-12-30 12:00:55 +0100

    travis: fix deploy

--- .travis/deploy-linux.sh
+++ .travis/deploy-linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # run make install
 make install DESTDIR=.
@@ -8,5 +8,5 @@ make install DESTDIR=.
 
 # build deb packages
 if [ "$UBUNTU_RELEASE" = "bionic" ]; then
-    checkinstall -y -D --install=no --fstrans=yes --pkgname=moonplayer --pkgversion=${TRAVIS_TAG} --pkgrelease=1~bionic --pkglicense=GPL_v3 --pkggroup=sound "--maintainer='coslyk <cos.lyk@gmail.com>'" --requires='ffmpeg, libmpv1, libqt5widgets5, libqt5network5, libqt5x11extras5, qml-module-qtquick2, qml-module-qtquick-controls2, qml-module-qtquick-dialogs, qml-module-qtquick-layouts, qml-module-qtquick-window2, qml-module-qt-labs-settings' --nodoc --exclude=/home
+    checkinstall -y -D --install=no --fstrans=yes --pkgname=moonplayer --pkgversion=${TRAVIS_TAG#v} --pkgrelease=1~bionic --pkglicense=GPL_v3 --pkggroup=sound "--maintainer='coslyk <cos.lyk@gmail.com>'" --requires='ffmpeg, libmpv1, libqt5widgets5, libqt5network5, libqt5x11extras5, qml-module-qtquick2, qml-module-qtquick-controls2, qml-module-qtquick-dialogs, qml-module-qtquick-layouts, qml-module-qtquick-window2, qml-module-qt-labs-settings' --nodoc --exclude=/home
 fi
