c2dfaa1be6002d179696d89181cb2273b756d038
YK Liu <cos.lyk@gmail.com>
2020-02-03 22:55:43 +0100

    travis: verbose output for linuxdeployqt

--- .travis/deploy-linux.sh
+++ .travis/deploy-linux.sh
@@ -7,4 +7,4 @@ find appdir/
 # Create appimage
 wget -Lo linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt.AppImage
-./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/
+./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -verbose=3
