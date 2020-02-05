e41bee1b05d972bf8debcc2fa0071b2b7f5c6efa
YK Liu <cos.lyk@gmail.com>
2020-02-05 17:21:01 +0100

    fix AppImage cannot run

--- .travis/deploy-linux.sh
+++ .travis/deploy-linux.sh
@@ -5,6 +5,7 @@ make install DESTDIR=appdir
 find appdir/
 
 # Create appimage
+export VERSION=${TRAVIS_TAG#v}
 curl -Lo linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt.AppImage
-./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/
+./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -exclude-libs=libdbus-1.so.3
