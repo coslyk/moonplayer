28232a322d74feaf715951420cdbc5942bfac97c
YK Liu <cos.lyk@gmail.com>
2020-04-15 18:46:07 +0200

    travis-ci: Update Qt installer

--- .travis/travis-install-qt.sh
+++ .travis/travis-install-qt.sh
@@ -1,36 +1,46 @@
-#!/bin/bash
-# Installs Qt on Windows for Travis CI.
-#
-# Copyright (C) 2019 Peter Wu <peter@lekensteyn.nl>
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-set -eu -o pipefail
+#!/bin/sh
 
 if [ -e "$QT5_BASE_DIR/bin/moc.exe" ] || [ -e "$QT5_BASE_DIR/bin/moc" ]; then
     echo "Found an existing Qt installation at $QT5_BASE_DIR"
     exit
 fi
 
-echo "Downloading the installer..."
-# https is of no use if it redirects to a http mirror...
 if [ "$TRAVIS_OS_NAME" = "windows" ]; then
-    QT_INSTALLER=~/qt_installer.exe
-    curl -Lo $QT_INSTALLER "http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe"
+    DESTDIR="C:\\Qt"
+    Base_URL="http://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5128/qt.qt5.5128.win64_msvc2017_64/5.12.8-0-202004051457qtbase-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
+    Declarative_URL="http://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5128/qt.qt5.5128.win64_msvc2017_64/5.12.8-0-202004051457qtdeclarative-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
+    ImageFormats_URL="http://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5128/qt.qt5.5128.win64_msvc2017_64/5.12.8-0-202004051457qtimageformats-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
+    QuickControls_URL="http://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5128/qt.qt5.5128.win64_msvc2017_64/5.12.8-0-202004051457qtquickcontrols-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
+    QuickControls2_URL="http://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5128/qt.qt5.5128.win64_msvc2017_64/5.12.8-0-202004051457qtquickcontrols2-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
+    Tools_URL="http://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5128/qt.qt5.5128.win64_msvc2017_64/5.12.8-0-202004051457qttools-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
 else
-    export QT_QPA_PLATFORM=minimal  # No X server for Qt installer
-    QT_INSTALLER=~/qt_installer.run
-    curl -Lo $QT_INSTALLER "http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run"
-    chmod +x $QT_INSTALLER
+    DESTDIR="$HOME/Qt"
+    Base_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtbase-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
+    Declarative_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtdeclarative-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
+    ImageFormats_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtimageformats-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
+    QuickControls_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtquickcontrols-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
+    QuickControls2_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtquickcontrols2-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
+    Tools_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qttools-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
+    X11Extras_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtx11extras-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
 fi
 
-echo "Installing..."
-# Run installer and save the installer output. To avoid hitting the timeout,
-# periodically print some progress. On error, show the full log and abort.
-$QT_INSTALLER --verbose --script .travis/qt-installer.qs |
-    tee ~/qt-installer-output.txt |
-    .travis/report-progress.sh ||
-    (cat ~/qt-installer-output.txt; exit 1)
+# Download
+curl -Lo base.7z "$Base_URL"
+curl -Lo declarative.7z "$Declarative_URL"
+curl -Lo imageformats.7z "$ImageFormats_URL"
+curl -Lo quickcontrols.7z "$QuickControls_URL"
+curl -Lo quickcontrols2.7z "$QuickControls2_URL"
 
-printf 'Installation size: '
-du -sm "$QT5_BASE_DIR" 2>&1 ||
-    (cat ~/qt-installer-output.txt; exit 1)
+# Extract
+[ -d $DESTDIR ] || mkdir $DESTDIR
+7z x -o$DESTDIR base.7z
+7z x -o$DESTDIR declarative.7z
+7z x -o$DESTDIR imageformats.7z
+7z x -o$DESTDIR quickcontrols.7z
+7z x -o$DESTDIR quickcontrols2.7z
+
+# Linux specific
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    curl -Lo x11extras.7z "$X11Extras_URL"
+    7z x -o$DESTDIR x11extras.7z
+fi
