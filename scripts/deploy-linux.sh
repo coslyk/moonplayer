a31e30480660ccdd20020ed229072faed894cc35
YK Liu <cos.lyk@gmail.com>
2020-09-04 21:31:07 +0200

    Bundle missing libraries in AppImage

--- scripts/deploy-linux.sh
+++ scripts/deploy-linux.sh
@@ -8,4 +8,4 @@ cp /usr/bin/ffmpeg ./appdir/usr/bin/
 export VERSION=${TRAVIS_TAG#v}
 curl -Lo linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt.AppImage
-./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -exclude-libs=libdbus-1.so.3
+./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -executable=appdir/usr/bin/ffmpeg -executable=appdir/usr/bin/moonplayer-hlsdl -exclude-libs=libdbus-1.so.3
