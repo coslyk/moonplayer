f4a0cf430adc4d42b7794ee821e2604eab1dd7e4
YK Liu <cos.lyk@gmail.com>
2020-10-07 18:43:56 +0200

    Update Qt to 5.15.1 on Linux

--- scripts/travis-install-qt.sh
+++ scripts/travis-install-qt.sh
@@ -15,14 +15,15 @@ if [ "$TRAVIS_OS_NAME" = "windows" ]; then
     Tools_URL="http://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5128/qt.qt5.5128.win64_msvc2017_64/5.12.8-0-202004051457qttools-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
 else
     DESTDIR="$HOME/Qt"
-    Base_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtbase-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
-    Declarative_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtdeclarative-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
-    ImageFormats_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtimageformats-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
-    QuickControls_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtquickcontrols-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
-    QuickControls2_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtquickcontrols2-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
-    Tools_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qttools-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
-    X11Extras_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456qtx11extras-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
-    ICU_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5128/qt.qt5.5128.gcc_64/5.12.8-0-202004051456icu-linux-Rhel7.2-x64.7z"
+    Base_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qtbase-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    Declarative_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qtdeclarative-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    ImageFormats_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qtimageformats-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    QuickControls_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qtquickcontrols-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    QuickControls2_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qtquickcontrols2-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    Tools_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qttools-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    X11Extras_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qtx11extras-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    Wayland_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110qtwayland-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+    ICU_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5151/qt.qt5.5151.gcc_64/5.15.1-0-202009071110icu-linux-Rhel7.2-x64.7z"
 fi
 
 # Download
@@ -45,8 +46,10 @@ curl -Lo tools.7z "$Tools_URL"
 # Linux specific
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     curl -Lo x11extras.7z "$X11Extras_URL"
+    curl -Lo wayland.7z "$Wayland_URL"
     curl -Lo icu.7z "$ICU_URL"
     7z x -o$DESTDIR x11extras.7z
+    7z x -o$DESTDIR wayland.7z
     7z x -o$DESTDIR icu.7z
 
     QT_VERSION=`ls $DESTDIR`
