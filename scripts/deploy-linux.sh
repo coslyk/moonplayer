77ce884fb6645b5326a9bc5c75263e8255d427ad
YK Liu <cos.lyk@gmail.com>
2020-10-07 19:30:00 +0200

    Fix AppImage build

--- scripts/deploy-linux.sh
+++ scripts/deploy-linux.sh
@@ -6,6 +6,8 @@ cp /usr/bin/ffmpeg ./appdir/usr/bin/
 
 # Create appimage
 export VERSION=${TRAVIS_TAG#v}
+EXTRA_PLUGINS="platforms/libqwayland-egl.so,platform/libqwayland-generic.so,platform/libqwayland-xcomposite-egl.so,platform/libqwayland-xcomposite-glx.so,wayland-decoration-client,wayland-graphics-integration-client,wayland-shell-integration"
+
 curl -Lo linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt.AppImage
-./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -executable=appdir/usr/bin/ffmpeg -executable=appdir/usr/bin/moonplayer-hlsdl -exclude-libs=libdbus-1.so.3 -extra-plugins=platforms,wayland-decoration-client,wayland-graphics-integration-client,wayland-shell-integration
+./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -executable=appdir/usr/bin/ffmpeg -executable=appdir/usr/bin/moonplayer-hlsdl -extra-plugins=$EXTRA_PLUGINS
