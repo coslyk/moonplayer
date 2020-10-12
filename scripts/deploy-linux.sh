b7183b421a61b08e3ccb5aa9a9317ddefcdf393a
YK Liu <cos.lyk@gmail.com>
2020-10-12 20:55:13 +0200

    Qt wayland in AppImage doesn't seem to work well

--- scripts/deploy-linux.sh
+++ scripts/deploy-linux.sh
@@ -6,8 +6,7 @@ cp /usr/bin/ffmpeg ./appdir/usr/bin/
 
 # Create appimage
 export VERSION=${TRAVIS_TAG#v}
-EXTRA_PLUGINS="platforms/libqwayland-egl.so,platforms/libqwayland-generic.so,platforms/libqwayland-xcomposite-egl.so,platforms/libqwayland-xcomposite-glx.so,wayland-decoration-client,wayland-graphics-integration-client,wayland-shell-integration"
 
 curl -Lo linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt.AppImage
-./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -executable=appdir/usr/bin/ffmpeg -executable=appdir/usr/bin/moonplayer-hlsdl -extra-plugins=$EXTRA_PLUGINS
+./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -executable=appdir/usr/bin/ffmpeg -executable=appdir/usr/bin/moonplayer-hlsdl
