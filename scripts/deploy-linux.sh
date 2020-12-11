dad7a29dbdb568e90618d2eb1dad9da9598a118b
Aven <turineaven@users.norepy.github.com>
2020-12-12 06:30:27 +0800

    Add wayland support to AppImage on Linux
    And supports local build of AppImage.

--- scripts/deploy-linux.sh
+++ scripts/deploy-linux.sh
@@ -7,6 +7,24 @@ cp /usr/bin/ffmpeg ./appdir/usr/bin/
 # Create appimage
 export VERSION=${TRAVIS_TAG#v}
 
-curl -Lo linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x linuxdeployqt.AppImage
-./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop -appimage -qmldir=src/qml/ -executable=appdir/usr/bin/ffmpeg -executable=appdir/usr/bin/moonplayer-hlsdl -exclude-libs=libdbus-1.so.3
+glibc_ver=$(ldd --version | sed -n "1s/^.* \([0-9.]*\)$/\1/p")
+if [ ${glibc_ver#*.} -ge 27 ]; then
+    no_check_glibc="-unsupported-allow-new-glibc"
+    copyright="/usr/share/doc/libc6/copyright"
+    install -p -T -D "$copyright" "appdir$copyright"
+fi
+
+if [ ! -x linuxdeployqt.AppImage ]; then
+    curl -Lo linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+    chmod a+x linuxdeployqt.AppImage
+fi
+
+wl_plugins="\
+platforms/libqwayland-generic.so,\
+platforms/libqwayland-egl.so,\
+wayland-shell-integration/libxdg-shell.so,\
+wayland-shell-integration/libxdg-shell-v6.so,\
+wayland-decoration-client/libbradient.so,\
+wayland-graphics-integration-client/libqt-plugin-wayland-egl.so"
+
+exec ./linuxdeployqt.AppImage appdir/usr/share/applications/*.desktop $no_check_glibc -appimage -qmldir=src/qml/ -executable=appdir/usr/bin/ffmpeg -executable=appdir/usr/bin/moonplayer-hlsdl -exclude-libs=libdbus-1.so.3 -extra-plugins="$wl_plugins"
--- scripts/travis-install-qt.sh
+++ scripts/travis-install-qt.sh
@@ -20,6 +20,7 @@ else
     QuickControls2_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5129/qt.qt5.5129.gcc_64/5.12.9-0-202006121744qtquickcontrols2-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
     Tools_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5129/qt.qt5.5129.gcc_64/5.12.9-0-202006121744qttools-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
     X11Extras_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5129/qt.qt5.5129.gcc_64/5.12.9-0-202006121744qtx11extras-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
+    Wayland_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5129/qt.qt5.5129.gcc_64/5.12.9-0-202006121744qtwayland-Linux-RHEL_7_4-GCC-Linux-RHEL_7_4-X86_64.7z"
     ICU_URL="http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5129/qt.qt5.5129.gcc_64/5.12.9-0-202006121744icu-linux-Rhel7.2-x64.7z"
 fi
 
@@ -41,15 +42,17 @@ curl -Lo tools.7z "$Tools_URL"
 # Linux specific
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     curl -Lo x11extras.7z "$X11Extras_URL"
+    curl -Lo wayland.7z "$Wayland_URL"
     curl -Lo icu.7z "$ICU_URL"
     7z x -o$DESTDIR x11extras.7z
+    7z x -o$DESTDIR wayland.7z
     7z x -o$DESTDIR icu.7z
 
     QT_VERSION=`ls $DESTDIR`
-    printf "[Paths]\nPrefix = $DESTDIR/$QT_VERSION/gcc_64" > $DESTDIR/$QT_VERSION/gcc_64/bin/qt.conf
+    printf "[Paths]\nPrefix = $DESTDIR/$QT_VERSION/gcc_64\n" > $DESTDIR/$QT_VERSION/gcc_64/bin/qt.conf
 
 # Windows specific
 elif [ "$TRAVIS_OS_NAME" = "windows" ]; then
     QT_VERSION=`ls /c/Qt`
-    printf "[Paths]\nPrefix = .." > /c/Qt/$QT_VERSION/msvc2017_64/bin/qt.conf
+    printf "[Paths]\nPrefix = ..\n" > /c/Qt/$QT_VERSION/msvc2017_64/bin/qt.conf
 fi
