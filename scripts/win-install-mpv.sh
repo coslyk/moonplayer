d02f09fae5196993f800c15a895cf9268cc73955
YK Liu <cos.lyk@gmail.com>
2021-10-27 10:16:06 +0200

    Action: use scripts to install Qt

---
+++ scripts/win-install-mpv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Download libmpv
+curl -Lo libmpv.7z https://deac-riga.dl.sourceforge.net/project/mpv-player-windows/libmpv/mpv-dev-x86_64-20210214-git-93066ff.7z
+
+# Extract libmpv
+7z x -olibmpv libmpv.7z
+
+# Produce .lib file
+cd libmpv
+mv include mpv
+lib /def:mpv.def /name:mpv-1.dll /out:mpv.lib /MACHINE:X64
+cd ..
