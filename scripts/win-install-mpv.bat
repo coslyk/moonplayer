cf42594a155548d7a7df0ef960dab746fb592453
YK Liu <cos.lyk@gmail.com>
2021-10-27 10:27:17 +0200

    Actions: fix cannot find windows compiler

--- scripts/win-install-mpv.bat
+++ scripts/win-install-mpv.bat
@@ -1,13 +1,11 @@
-#!/bin/sh
-
-# Download libmpv
+: Download libmpv
 curl -Lo libmpv.7z https://deac-riga.dl.sourceforge.net/project/mpv-player-windows/libmpv/mpv-dev-x86_64-20210214-git-93066ff.7z
 
-# Extract libmpv
+: Extract libmpv
 7z x -olibmpv libmpv.7z
 
-# Produce .lib file
+: Produce .lib file
 cd libmpv
-mv include mpv
+rename include mpv
 lib /def:mpv.def /name:mpv-1.dll /out:mpv.lib /MACHINE:X64
 cd ..
