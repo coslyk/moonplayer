e42249232b8f81b3dd0c7281930480e4ff8798d0
YK Liu <cos.lyk@gmail.com>
2022-03-06 16:10:55 +0800

    Update libmpv's url for windows build

--- scripts/win-install-mpv.bat
+++ scripts/win-install-mpv.bat
@@ -1,5 +1,5 @@
 : Download libmpv
-curl -Lo libmpv.7z https://deac-riga.dl.sourceforge.net/project/mpv-player-windows/libmpv/mpv-dev-x86_64-20210214-git-93066ff.7z
+curl -Lo libmpv.7z https://jaist.dl.sourceforge.net/project/mpv-player-windows/libmpv/mpv-dev-x86_64-20220227-git-b1fb4b7.7z
 
 : Extract libmpv
 7z x -olibmpv libmpv.7z
