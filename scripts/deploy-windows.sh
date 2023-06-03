de226f18dc4cb54aa97d4c7a4e163b491a050167
Yikun Liu <cos.lyk@gmail.com>
2023-06-03 17:21:23 +0800

    Update windows dependencies

--- scripts/deploy-windows.sh
+++ scripts/deploy-windows.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export PATH=/c/Qt/6.2.4/msvc2019_64/bin:/c/Qt/5.15.2/msvc2019_64/bin:$PATH
-
 # Copy binary files
 mkdir moonplayer
 cp src/Release/moonplayer.exe src/scripts/update-parsers.ps1 libmpv/mpv-2.dll moonplayer/
@@ -10,12 +8,12 @@ cp src/Release/moonplayer.exe src/scripts/update-parsers.ps1 libmpv/mpv-2.dll mo
 windeployqt moonplayer/moonplayer.exe --qmldir src/qml
 
 # Bundle OpenSSL
-curl -Lo openssl.7z https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/tools_openssl_x64/qt.tools.openssl.win_x64/1.1.1-10openssl_1.1.1j_prebuild_x64.7z
+curl -Lo openssl.7z https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/tools_openssl_x64/qt.tools.openssl.win_x64/1.1.1-17openssl_1.1.1q_prebuild_x64.7z
 7z e openssl.7z -omoonplayer Tools/OpenSSL/Win_x64/bin/*.dll
 
 # Bundle ffmpeg
-curl -Lo ffmpeg.7z https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.1.1-essentials_build.7z
-7z e ffmpeg.7z -omoonplayer ffmpeg-5.1.1-essentials_build/bin/ffmpeg.exe
+curl -Lo ffmpeg.7z https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.1.2-essentials_build.7z
+7z e ffmpeg.7z -omoonplayer ffmpeg-5.1.2-essentials_build/bin/ffmpeg.exe
 
 # Bundle hlsdl
 curl -Lo hlsdl.7z https://rwijnsma.home.xs4all.nl/files/hlsdl/hlsdl-0.27-883acbd-win32-static-xpmod-sse.7z
